9b9c251f741076d1a6245c38c5fff43024f43e54
Florin Ciodaru <7295029+ciody@users.noreply.github.com>
2023-01-24 16:17:46 -0500

    unit testing support (#2)
    add unit tests that validate the behaviour of lettersum function
    
    Signed-off-by: ciody <7295029+ciody@users.noreply.github.com>
    
    Signed-off-by: ciody <7295029+ciody@users.noreply.github.com>

--- src/bin/letters-value-sum.rs
+++ src/bin/letters-value-sum.rs
@@ -12,3 +12,39 @@ fn lettersum(s: &str) -> i32 {
         .map(|c| c as i32 - a as i32 + 1)
         .sum()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lettersum_a() -> Result<(), String> {
+        let letters = "a";
+        assert_eq!(lettersum(letters), 1);
+        Ok(())
+    }
+    #[test]
+    fn test_lettersum_z() -> Result<(), String> {
+        let letters = "z";
+        assert_eq!(lettersum(letters), 26);
+        Ok(())
+    }
+    #[test]
+    fn test_lettersum_az() -> Result<(), String> {
+        let letters = "az";
+        assert_eq!(lettersum(letters), 27);
+        Ok(())
+    }
+    #[test]
+    fn test_lettersum_empty() -> Result<(), String> {
+        let letters = "";
+        assert_eq!(lettersum(letters), 0);
+        Ok(())
+    }
+    #[test]
+    fn test_lettersum_nonaz_are_ignored() -> Result<(), String> {
+        let letters = "a+B z";
+        assert_eq!(lettersum(letters), 27);
+        Ok(())
+    }
+}
